3a9d92da775b666acfba21c1f6b463fd4b17c693
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-04-25 13:24:54 +0530

    Add files via upload

---
+++ Week1/Day1/First_Program.java
@@ -0,0 +1,13 @@
+package org.testleaf.week1.day1;
+
+//public - Access Modifier/Access Specifier
+//class - keyword
+//First_Program - ClassName
+// {  Java Codes }  - Class Body
+
+public class First_Program {
+    //declare main method
+    public static void main(String[] args) {
+        System.out.println("This is by shortcut");
+    }
+}
---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,43 @@
+package org.testleaf.week1.day1;
+
+public class LearnDatatypes {
+public static void main(String[] args) {
+   
+    //byte
+    byte age=32;      //32 will be assigned to the varaible age
+
+    //short
+   short ageOfPerson=130;
+
+   //int
+   int amount=50000;
+
+   //long
+   long mobileNumber=8925411170L;
+
+   //float
+    float rateOfInterest=11.5F;
+
+   float rupee=1.123456789f;
+   //System.out.println(rupee);
+   
+   double dollar=1.123456789;
+   //System.out.println(dollar);
+
+   //char
+     char logoOfCompany='T';
+
+   //boolean
+   boolean didYouVoted=true;
+
+   //String
+   String name="Vineeth";
+  System.out.println(name);
+
+//my name with age
+System.out.println(name+" "+age);
+
+//The name is Vineeth
+System.out.println("My name is "+name);
+     }
+}
